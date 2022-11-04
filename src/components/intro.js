a9e749f0be39176de5895be56be7357deb6c3118
danang-pgds <danang.pgds@gmail.com>
2022-11-04 16:55:23 +0800

    Update intro.js

--- src/components/intro.js
+++ src/components/intro.js
@@ -3,7 +3,9 @@ function Intro(props) {
       <div >
         <h1>Hi, {props.name}</h1>
         <h1>Test build app do</h1>
+        {/* <p>ini intro</p> */}
         <p>ini intro</p>
+        <p>ini intro 2</p>
       </div>
     );
   }
